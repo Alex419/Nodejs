2fc25753c686e4d168f52900153b10c9096c6549
Alex419 <67164221+Alex419@users.noreply.github.com>
2024-04-28 18:24:46 -0400

    Update app2.js

--- app2.js
+++ app2.js
@@ -9,48 +9,48 @@ var port = process.env.PORT || 3000;
 // var port = 8080;
 
 
-        try {
-            const parsedUrl = url.parse(req.url, true);
-            const lookupKey = parsedUrl.query.lookup_key;
+        // try {
+        //     const parsedUrl = url.parse(req.url, true);
+        //     const lookupKey = parsedUrl.query.lookup_key;
     
-            console.log("Lookup Key:", lookupKey); 
+        //     console.log("Lookup Key:", lookupKey); 
     
-            const client = await MongoClient.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-            const db = client.db('Nodejs');
+        //     const client = await MongoClient.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        //     const db = client.db('Nodejs');
     
-            // const documents = await db.collection('places').find({ place: lookupKey }).toArray(); // Retrieve all documents
-            // const documents = await db.collection('places').find({ zips: lookupKey }).toArray();
-            // const documents = await db.collection('places').findOne({ $or: [{ place: lookupKey }, { zips: lookupKey }] });
+        //     // const documents = await db.collection('places').find({ place: lookupKey }).toArray(); // Retrieve all documents
+        //     // const documents = await db.collection('places').find({ zips: lookupKey }).toArray();
+        //     // const documents = await db.collection('places').findOne({ $or: [{ place: lookupKey }, { zips: lookupKey }] });
 
-            if (lookupKey && isNaN(lookupKey.charAt(0))) { 
-                documents = await db.collection('places').findOne({ place: lookupKey });
-            } else {
-                documents = await db.collection('places').findOne({ zips: lookupKey });
-            }
-            await client.close();
+        //     if (lookupKey && isNaN(lookupKey.charAt(0))) { 
+        //         documents = await db.collection('places').findOne({ place: lookupKey });
+        //     } else {
+        //         documents = await db.collection('places').findOne({ zips: lookupKey });
+        //     }
+        //     await client.close();
 
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write("<h1>Information about where you chose!</h1>");
-            if (documents) {
-                const place = documents.place;
-                const zips = documents.zips.join(', ');
-                res.write("<p><strong>Place:</strong> " + place + "</p>");
-                res.write("<p><strong>Zip Codes:</strong> " + zips + "</p>");
-            } else {
-                res.write("<p>Sorry! No place found with the given information.</p>");
-            }
+        //     res.writeHead(200, { 'Content-Type': 'text/html' });
+        //     res.write("<h1>Information about where you chose!</h1>");
+        //     if (documents) {
+        //         const place = documents.place;
+        //         const zips = documents.zips.join(', ');
+        //         res.write("<p><strong>Place:</strong> " + place + "</p>");
+        //         res.write("<p><strong>Zip Codes:</strong> " + zips + "</p>");
+        //     } else {
+        //         res.write("<p>Sorry! No place found with the given information.</p>");
+        //     }
 
-            res.write("<a href='/'>Back to Search Page</a>");
-            res.end();
+        //     res.write("<a href='/'>Back to Search Page</a>");
+        //     res.end();
 
-            console.log("All Documents:", documents);
+        //     console.log("All Documents:", documents);
     
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-        } catch (error) {
-            console.error('Error processing request:', error);
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Internal Server Error');
-        }
+        //     res.writeHead(200, { 'Content-Type': 'application/json' });
+        // } catch (error) {
+        //     console.error('Error processing request:', error);
+        //     res.writeHead(500, { 'Content-Type': 'text/plain' });
+        //     res.end('Internal Server Error');
+        // }
 
 http.createServer(async function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
